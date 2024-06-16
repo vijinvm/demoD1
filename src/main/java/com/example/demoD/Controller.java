1601929af02f960cafb2a7362542f6136f243cab
vijinvm <121943978+vijinvm@users.noreply.github.com>
2024-06-17 05:00:43 +0530

    hi

--- src/main/java/com/example/demoD/Controller.java
+++ src/main/java/com/example/demoD/Controller.java
@@ -8,11 +8,11 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api")
 public class Controller {
 @Autowired
 private CheckinRepository p;
-@GetMapping()
+@GetMapping("/checkin")
 public Page<Checkin> a1(@RequestParam int page,
 		@RequestParam int size){
 	return p.findAll(PageRequest.of(page, size));
@@ -22,28 +22,28 @@ public List<Checkin> a2(@RequestParam String sortBy){
 	Sort sort =Sort.by(sortBy);
 	return p.findAll(sort);
 }
-@GetMapping()
+@GetMapping("/checkin/{id}")
 public Checkin  a3(@PathVariable int id) {
 	return p.findById(id).orElse(null);
 }
-@PostMapping()
+@PostMapping("/checkin")
 public Checkin a4(@RequestBody Checkin checkin  ) {
 	return p.save(checkin);
 }
-@PutMapping("/categories/{id}")
+@PutMapping("/checkin/{id}")
 public Checkin a5(@PathVariable int id,@RequestBody Checkin  checkin) {
 	return p.findById(id).map(e->{e.setName(checkin.getName());
 	return p.save(e);
 })
 			.orElse(null);
 }
-@DeleteMapping()
+@DeleteMapping("/checkin/{id}")
 public void a6(@PathVariable int id) {
 	p.deleteById(id);
 }
 @Autowired
 private GuestRepository q;
-@GetMapping()
+@GetMapping("/guest/{id}")
 public Page<Guest> b1(@RequestParam int page,
 		@RequestParam int size){
 	return q.findAll(PageRequest.of(page, size));
@@ -53,22 +53,22 @@ public List<Guest> b2(@RequestParam String sortBy){
 	Sort sort =Sort.by(sortBy);
 	return q.findAll(sort);
 }
-@GetMapping()
+@GetMapping("/guest/{id}")
 public Guest  b3(@PathVariable int id) {
 	return q.findById(id).orElse(null);
 }
-@PostMapping()
+@PostMapping("/guest")
 public Guest b4(@RequestBody Guest guest  ) {
 	return q.save(guest);
 }
-@PutMapping("/categories/{id}")
-public Guest b5(@PathVariable int id,@RequestBody Guest  Guest) {
-	return q.findById(id).map(e->{e.setName(Guest.getName());
+@PutMapping("/guest/{id}")
+public Guest b5(@PathVariable int id,@RequestBody Guest  guest) {
+	return q.findById(id).map(e->{e.setName(guest.getName());
 	return q.save(e);
 })
 			.orElse(null);
 }
-@DeleteMapping()
+@DeleteMapping("/guest/{id}")
 public void b6(@PathVariable int id) {
 	q.deleteById(id);
 }
